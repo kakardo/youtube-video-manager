62788e9003a2da3240ae74aaba46e8c73b52346f
kakardo <kardo.rostam@gmail.com>
2026-05-11 17:14:59 +0200

    Learned to use flush()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
     println!("Hej hej världen!");
+
+    let mut choice = String::new();
+    print!("Write anything: ");
+
+
+    io::stdout().flush().expect("Failed to flush stdout");
+    io::stdin().read_line(&mut choice).expect("Failed to read line");
+    
+    println!("You wrote: {}", choice.trim());
 }
