e242bb083cfd6a3952a03986ae3dcb94a1a60ca7
kakardo <kardo.rostam@gmail.com>
2026-05-12 17:04:35 +0200

    Working while loop

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,33 @@ use std::io;
 use std::io::Write;
 
 fn main() {
-    println!("VIDEO MANAGER MENU");
-    println!("(1) Show video");
-    println!("(2) Mark video as watched");
-    println!("(3) Exit");
-
     let mut choice = String::new();
-    print!("Option: ");
+
+    while choice.trim() != "3" {
+        println!("\nVIDEO MANAGER MENU");
+        println!("(1) Show video");
+        println!("(2) Mark video as watched");
+        println!("(3) Exit");
+
+        choice.clear(); // remove previous option
+        print!("Option: ");
+        
+        io::stdout()
+            .flush()
+            .expect("Failed to flush stdout");
     
-    io::stdout()
-        .flush()
-        .expect("Failed to flush stdout");
-   
-    io::stdin()
-        .read_line(&mut choice)
-        .expect("Failed to read line");
-    //println!("You wrote: {}", choice.trim());d
-    
-    // String match
-    match choice.trim() {
-        "1" => println!("Show video"),
-        "2" => println!("Mark video as watched"),
-        "3" => println!("Exit"),
-        _ => println!("Invalid option!"),
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read line");
+        //println!("You wrote: {}", choice.trim());d
+        
+        // String match
+        match choice.trim() {
+            "1" => println!("Show video"),
+            "2" => println!("Mark video as watched"),
+            "3" => println!("Exit"),
+            _ => println!("Invalid option!"),
+        }
     }
+
 }
