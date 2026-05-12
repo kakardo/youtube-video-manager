580a9845b7f00fb0b85de8cb6736ddc6667a720d
kakardo <kardo.rostam@gmail.com>
2026-05-12 16:39:45 +0200

    Finally working string matching

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,28 @@ use std::io;
 use std::io::Write;
 
 fn main() {
-    println!("Hej hej världen!");
+    println!("VIDEO MANAGER MENU");
+    println!("(1) Show video");
+    println!("(2) Mark video as watched");
+    println!("(3) Exit");
 
     let mut choice = String::new();
-    print!("Write anything: ");
-
-
-    io::stdout().flush().expect("Failed to flush stdout");
-    io::stdin().read_line(&mut choice).expect("Failed to read line");
+    print!("Option: ");
     
-    println!("You wrote: {}", choice.trim());
+    io::stdout()
+        .flush()
+        .expect("Failed to flush stdout");
+   
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read line");
+    //println!("You wrote: {}", choice.trim());d
+    
+    // String match
+    match choice.trim() {
+        "1" => println!("Show video"),
+        "2" => println!("Mark video as watched"),
+        "3" => println!("Exit"),
+        _ => println!("Invalid option!"),
+    }
 }
